7e03d08420252079a8b972ab71fc63f9cc37565d
rachelnoireau <rachel.noireau@uha.fr>
2018-11-09 18:27:20 -0500

    classe agent

---
+++ agent.py
@@ -0,0 +1,33 @@
+from enum import Enum
+
+class Action(Enum):
+    UP = 1
+    RIGHT = 2
+    DOWN = 3
+    LEFT = 4
+    USE_CRISTAL = 5
+    TAKE_CRISTAL = 6
+    TAKE_PORTAL = 7
+
+
+
+class Agent:
+
+    def __init__(self):
+        self.posX = 0
+        self.posY = 0
+        self.nbr_dead = 0
+        self.nbr_cristal = 0
+
+
+
+    def is_dead(self):
+        self.nbr_dead += 1
+
+    def get_pos(self):
+        return (self.posX , self.posY)
+
+
+    def go_right(self): #juste pour tester
+        self.posY += 1
+
--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-from area import Area
 from wood import Wood
 from display import Display
+from agent import Agent
 
 class Simulation:
     def __init__(self):
-        self.size = 3
-
-        self.wood = Wood(self.size)
+        self.agent = Agent(self)
         self.display = Display(self.size)
 
+        self.size = 3
+        self.wood = Wood(self.size)
+        
         self.display.add_grid(self.wood.get_grid())
         self.display.create_button()
         self.display.start_loop()
